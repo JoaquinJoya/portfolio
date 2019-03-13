4b4a9d5a49d3364b77ec3f049214ab35b484fed1
Joaquin Joya <contact@joaquinjoyareal.com>
2019-03-12 23:00:22 -0600

    Fixng Error Footer

--- src/components/Footer/CTAFooter.js
+++ src/components/Footer/CTAFooter.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Wrapper } from "../styledComponents/styledComponents"
+import { Wrapper } from "../styledComponents/styledComponents.js"
 import { Title, Text } from "./CTAFooter.style"
 const CTAFooter = () => (
     
